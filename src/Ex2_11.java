6ea3581eb1819a406c52f02ea3ca47100fc28e01
suunydream <78350885+suunydream@users.noreply.github.com>
2021-11-06 15:32:53 +0900

    자바의 정석 코드 직접 짜보기

---
+++ src/Ex2_11.java
@@ -0,0 +1,8 @@
+public class Ex2_11 {
+    public static void main(String[] args) {
+        short sMin = -32768, sMax = 32767;
+        char cMin = 0,       cMax = 65535;
+
+
+    }
+}
--- src/Ex2_4.java
+++ src/Ex2_4.java
@@ -1,6 +1,6 @@
 public class Ex2_4 {
     public static void main(String[] args) {
         int x = 5;
-        Syst
+        System.out.println(x);
     }
 }
