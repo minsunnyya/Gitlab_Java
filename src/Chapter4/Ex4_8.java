4c7ab668a6195d12dccf83f044dc5f583e8ad675
minsunnyya <alstjs6471@daum.net>
2021-11-07 17:34:08 +0900

    Chapter4 코딩연습

---
+++ src/Chapter4/Ex4_8.java
@@ -0,0 +1,10 @@
+package Chapter4;//for문
+
+public class Ex4_8 {
+    public static void main(String[] args) {
+        for (int i=1; i <= 3; i++) {//i는 1부터 3까지 1씩증가하며 반복
+            System.out.println("Hello");
+        }
+
+    }
+}
