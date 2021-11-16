f5e8d1e817cc1cfe64b5926742898c58fe1f90f4
minsunnyya <alstjs6471@daum.net>
2021-11-16 15:29:46 +0900

    내부클래스 연습

--- src/Chapter7_Extends/Exercise7_1.java
+++ src/Chapter7_Extends/Exercise7_1.java
@@ -3,19 +3,17 @@ package Chapter7_Extends;
 class SutdaDeck {
     final int CARD_NUM = 20;
     SutdaCard[] cards = new SutdaCard[CARD_NUM];
-
-    SutdaDeck() {
-        //섯다카드 20장을 담는 값을 초기화
-       for(int i = 0; i<cards.length;i++){
-           int num =
+//
+//    SutdaDeck() {
+//        //섯다카드 20장을 담는 값을 초기화
+//       for(int i = 0; i<cards.length;i++){
+//           int num =
        }
         //단, 섯다 카드는 1~10까지의 숫자가 적힌 카드가 한 쌍씩있고
 
         //숫자 1,3,8인 경우는 둘 중 한 장은 광이어야한다
         //즉 SutdaCard의 인스턴스 변수 isKwang의 값이 True여야한다
-    }
 
-}
 
 class SutdaCard {
     int num;
