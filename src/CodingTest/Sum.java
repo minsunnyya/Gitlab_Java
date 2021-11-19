1d2926e91e2e4268f86476e294480f0e55811145
minsunnyya <alstjs6471@daum.net>
2021-11-19 18:31:43 +0900

    두 정수 사이의 합 : 조건 연산자 이용해서 품

---
+++ src/CodingTest/Sum.java
@@ -0,0 +1,21 @@
+package CodingTest;
+
+class Solution {
+    public long solution(int a, int b) {
+        long answer = 0;
+
+        for(int x = ((a < b)?a:b);x<=((a<b)?b:a);x++)
+            answer+= x;
+
+        return answer;
+    }
+}
+
+
+
+/*풀이방법
+1.x의 값을 구하기 위해 조건 연산자 사용
+만약 a = 2이고 b가 4이가 참일때 x에 2저장 , x의 범위에 4 저장 해서
+2+3+4더하게 만듬
+*
+* */
