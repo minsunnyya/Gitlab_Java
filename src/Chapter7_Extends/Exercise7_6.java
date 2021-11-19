a6020a9880e37c3c9b6363007d7b115df112148f
minsunnyya <alstjs6471@daum.net>
2021-11-19 16:13:49 +0900

    7단원 최종 연습문제 풀이

--- src/Chapter7_Extends/Exercise7_6.java
+++ src/Chapter7_Extends/Exercise7_6.java
@@ -1,15 +1,15 @@
 package Chapter7_Extends;
 
-class Outer{
-    class Inner{
+class Outer001{
+    class Inner1{
         int iv = 100;
     }
 }
 
 public class Exercise7_6 {
     public static void main(String[] args) {
-        Outer o = new Outer();//외부클래스 인스턴스 생성
-        Outer.Inner i = o.new Inner();//외부 클래스 안의 내부클래스 인스턴스 생성
+        Outer001 o = new Outer001();//외부클래스 인스턴스 생성
+        Outer001.Inner1 i = o.new Inner1();//외부 클래스 안의 내부클래스 인스턴스 생성
         System.out.println(i.iv);
     }
 }
