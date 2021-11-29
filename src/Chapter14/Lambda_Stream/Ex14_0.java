a8e6efa880dc35c38026d3fe480bbbea5b26b9af
minsunnyya <alstjs6471@daum.net>
2021-11-29 18:13:12 +0900

    로또 번호 만들기

--- src/Chapter14/Lambda_Stream/Ex14_0.java
+++ src/Chapter14/Lambda_Stream/Ex14_0.java
@@ -3,23 +3,12 @@ package Chapter14.Lambda_Stream;
 import java.util.function.Function;
 import java.util.*;
 import java.util.function.Supplier;
+import java.util.stream.IntStream;
 
 public class Ex14_0 {
     public static void main(String[] args) {
 
-
-        //Function<Integer,int[]> f = i->new int[i]; //람다식
-        Function<Integer, int[]> f = int[]::new;
-        System.out.println(f.apply(4).length);//매서드 참조
-
-
-    }
-}
-
-class MyClass{
-    int iv;
-
-    MyClass(int iv){//생성자
-        this.iv=iv;
+        IntStream intStream = new Random().ints(1, 46);
+        intStream.distinct().limit(6).sorted().forEach(i -> System.out.print(i + ","));
     }
 }
