a9b49554541edf4a5de6e5c438997c2379022d0f
minsunnyya <alstjs6471@daum.net>
2021-11-29 17:58:00 +0900

    배열 메서드 참조 해보기

--- src/Chapter14/Lambda_Stream/Ex14_0.java
+++ src/Chapter14/Lambda_Stream/Ex14_0.java
@@ -6,15 +6,11 @@ import java.util.function.Supplier;
 
 public class Ex14_0 {
     public static void main(String[] args) {
-        //Supplier 입력없고 출력만 있음
-       // Supplier<MyClass> s = ()->new MyClass();//클래스 이름이 MyClass
-//        Supplier<MyClass> s = MyClass::new;
-//        Function<Integer, MyClass> s = (i)-> new MyClass(i);
-        Function<Integer, MyClass> s =MyClass::new;
-//        Function<int , MyClass> s = MyClass::iv;//Myclass에 입력도 만들고 출력도 만듬  Function써야함
-        MyClass mc = s.apply(100);
-        System.out.println(mc.iv);
-        //System.out.println(s.apply(100).iv);
+
+
+        //Function<Integer,int[]> f = i->new int[i]; //람다식
+        Function<Integer, int[]> f = int[]::new;
+        System.out.println(f.apply(4).length);//매서드 참조
 
 
     }
