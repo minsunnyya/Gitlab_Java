841cff50a1e7d676cc77385a57b7311b9e92c6fe
minsunnyya <alstjs6471@daum.net>
2021-11-29 15:56:45 +0900

    14.함수형 인터페이스 타입의 매개변수, 반환타입

---
+++ src/Chapter14/Lambda_Stream/Ex14_1.java
@@ -0,0 +1,38 @@
+package Chapter14.Lambda_Stream;
+
+@FunctionalInterface
+interface MyFunction {
+    void run();
+}
+
+public class Ex14_1 {
+    static void execute(MyFunction f){//매개 변수 타입이 MyFunction인 타입
+        f.run();
+    }
+
+    static MyFunction getMyFunction(){
+        MyFunction f =()-> System.out.println("f3.run()");//()참조변수 없다는 뜻
+        return f;
+
+    }
+
+    public static void main(String[] args) {
+        MyFunction f1 = ()->System.out.println("f1.run()");
+
+        MyFunction f2 = new MyFunction() {
+            @Override
+            public void run() {
+                System.out.println("f2.run()");
+            }
+        };
+
+        MyFunction f3 = getMyFunction();
+
+        f1.run();
+        f2.run();
+        f3.run();
+
+        execute(f1);
+        execute(()-> System.out.println("run()"));
+    }
+}
