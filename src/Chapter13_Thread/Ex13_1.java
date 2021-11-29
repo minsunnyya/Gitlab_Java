7ae95f997fd96fac6c18b9b5bc9188f06d09d810
minsunnyya <alstjs6471@daum.net>
2021-11-29 17:32:49 +0900

    쓰레드 예제

--- src/Chapter13_Thread/Ex13_1.java
+++ src/Chapter13_Thread/Ex13_1.java
@@ -1,32 +1,32 @@
-package Chapter13_Thread;
-
-public class Ex13_1 {
-    public static void main(String[] args) {
-        ThreadEx1_1 t1 = new ThreadEx1_1();
-
-<<<<<<< HEAD
-        Runnable r = new T
-=======
-        Runnable r = new ThreadEx1_2();
-        Thread t2 = new Thread(r);
-
-        t1.start();
-        t2.start();
-    }
-}
-
-class ThreadEx1_1 extends Thread{
-    public void run(){
-        for(int i = 0 ; i<5; i++){
-            System.out.println(getName());
-        }
-    }
-}
-class ThreadEx1_2 implements Runnable{
-    public void run(){
-        for(int i = 0; i < 5; i ++){
-            System.out.println(Thread.currentThread().getName());
-        }
->>>>>>> 23d693c693f7f2a08cb16a8c5564c09bb4579865
-    }
-}
+//package Chapter13_Thread;
+//
+//public class Ex13_1 {
+//    public static void main(String[] args) {
+//        ThreadEx1_1 t1 = new ThreadEx1_1();
+//
+//<<<<<<< HEAD
+//        Runnable r = new T
+//=======
+//        Runnable r = new ThreadEx1_2();
+//        Thread t2 = new Thread(r);
+//
+//        t1.start();
+//        t2.start();
+//    }
+//}
+//
+//class ThreadEx1_1 extends Thread{
+//    public void run(){
+//        for(int i = 0 ; i<5; i++){
+//            System.out.println(getName());
+////        }
+//    }
+//}
+//class ThreadEx1_2 implements Runnable{
+//    public void run(){
+//        for(int i = 0; i < 5; i ++){
+//            System.out.println(Thread.currentThread().getName());
+//        }
+//>>>>>>> 23d693c693f7f2a08cb16a8c5564c09bb4579865
+//    }
+//}
