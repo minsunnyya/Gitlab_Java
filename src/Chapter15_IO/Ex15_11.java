4230db685664152742e004603cedd78a7b84a600
minsunnyya <alstjs6471@daum.net>
2021-11-30 19:09:18 +0900

    BufferReader,BufferedWriter

---
+++ src/Chapter15_IO/Ex15_11.java
@@ -0,0 +1,21 @@
+package Chapter15_IO;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Ex15_11 {
+    public static void main(String[] args) {
+        try{
+            FileReader fr = new FileReader("src/Chapter15_IO/Ex15_11.java");
+            BufferedReader br = new BufferedReader(fr);
+
+            String line = "";
+            for (int i = 1; (line=br.readLine())!=null;i++){
+                if(line.indexOf(";")!=-1)
+                    System.out.println(i+":"+line);
+            }
+            br.close();
+        }catch (IOException e){}
+    }
+}
