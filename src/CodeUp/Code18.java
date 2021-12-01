0e12d67c475f66db0c31847181247238d12068dd
minsunnyya <alstjs6471@daum.net>
2021-12-01 16:10:42 +0900

    codeup코딩테스트

--- src/CodeUp/Code18.java
+++ src/CodeUp/Code18.java
@@ -5,6 +5,11 @@ import java.util.Scanner;
 public class Code18 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+        System.out.println("시간을 입력하세요");
+        int hour = sc.nextInt();
+        System.out.println("분을 입력하세요");
+        int min = sc.nextInt();
+        System.out.printf("%d:%d",hour,min);
+
     }
 }
