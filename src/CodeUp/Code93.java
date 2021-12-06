efc3263ed8d0ff4e8d8f4ced89034c72fd055c81
suunydream <78350885+suunydream@users.noreply.github.com>
2021-12-07 00:36:01 +0900

    93번 문제->모름

---
+++ src/CodeUp/Code93.java
@@ -0,0 +1,20 @@
+package CodeUp;
+
+import java.util.Scanner;
+
+public class Code93 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("숫자를 입력하세요>>");
+        int a = sc.nextInt();
+
+
+
+
+
+        }
+
+        }
+
+
+    }
