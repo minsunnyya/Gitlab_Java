eb8679c6ae79a32f878f36630e4bc066be12c44b
suunydream <78350885+suunydream@users.noreply.github.com>
2021-12-11 01:25:18 +0900

    최솟값 구하기

--- src/CodeUp/Code95.java
+++ src/CodeUp/Code95.java
@@ -8,17 +8,22 @@ public class Code95 {
         Scanner sc = new Scanner(System.in);
         int b = sc.nextInt();
         int[] a = new int[b];//0으로 다 초기화 되어있음
-
-        for (int i = 0; i <b; i++) {//1234
-            int num = sc.nextInt();//계속 반복 되면서 값 치게끔함
-            a[i]=num;
+        int c = 0;
 
 
-
-        }
-        for (int i = 0; i <=a.length; i++){
-            System.out.println(a[i]);
+        for (int i = 0; i < b; i++) {
+            a[i] = sc.nextInt();
+            c = a[0];
 
         }
+        for (int i = 0; i < b; i++) {
+            if (a[i] < c)
+                c = a[i];
+
+        }
+
+
+        System.out.println(c);
     }//end main()
+
 }
