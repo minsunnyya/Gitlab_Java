4a053056c570f69bc38d2b936e5bd5b2ba0d6d34
minsunnyya <72340175+minsunnyya@users.noreply.github.com>
2022-01-07 18:02:45 +0900

    게시판2
    게시판..화팅

---
+++ ms-blog/src/test/java/com/pmsplus/msblog/MsBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pmsplus.msblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
